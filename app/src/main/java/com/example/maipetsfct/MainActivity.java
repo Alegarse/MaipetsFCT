586abeacaac1b08a826d79c8d3966ca828c808a0
Alejandro Garcia <agscm2@gmail.com>
2020-04-13 12:22:55 +0200

    Uploading

--- app/src/main/java/com/example/maipetsfct/MainActivity.java
+++ app/src/main/java/com/example/maipetsfct/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.maipetsfct;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button letsGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instancio el boton y configuro su listener
+        letsGo = findViewById(R.id.go);
+
+        letsGo.setOnClickListener(v -> {
+            Intent start = new Intent(MainActivity.this, PostMainActivity.class);
+            startActivity(start);
+        });
     }
 }
