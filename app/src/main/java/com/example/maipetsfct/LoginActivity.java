fa95d4964db3269ec9471a9e6e705f91bd46d759
Alejandro Garcia <agscm2@gmail.com>
2020-05-09 20:26:26 +0200

    Uploading

--- app/src/main/java/com/example/maipetsfct/LoginActivity.java
+++ app/src/main/java/com/example/maipetsfct/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.maipetsfct;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,5 +23,16 @@ public class LoginActivity extends AppCompatActivity {
 
         login = findViewById(R.id.btnLogin);
         cancel = findViewById(R.id.btnCancel);
+
+        // Escuchador para el botón Cancelar
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cancelamos y volvemos al principal
+                setResult(RESULT_CANCELED);
+                finish();
+                return;
+            }
+        });
     }
 }
