51eded83b98d9e678cdbe34fa9c536f1ef97f073
Alejandro Garcia <agscm2@gmail.com>
2020-06-04 17:27:53 +0200

    Uploading WebPage

--- webPage/js/mijs.js
+++ webPage/js/mijs.js
@@ -1,21 +1,5 @@
 window.onload = function () {
 
-    var s = Snap("#svg");
-
-    Snap.load("icono.svg", function (f) {
-
-        tapa = f.select("#tapa");
-        container = f.select("#papelera");
-        container.hover(hoverover, hoverout);
-        s.append(f);
-    });
-
-    var hoverover = function () {
-        tapa.animate({transform: "t0,-15"},800,mina.bounce);
-        document.getElementById("sonido").play();
-    }; //And return to original position when not hovered over
-
-    var hoverout = function () {
-        tapa.animate({transform: ""},800,mina.bounce);
-    };
+    document.getElementById("sonido").play();
+    
 };
